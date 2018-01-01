dc3bd90babc37902a6910bbd1b1e86cac05e33d4
Donald Moore <donmoore26762671@gmail.com>
2018-01-01 00:37:31 -0600

    Building Main and Grid components of game of life

---
+++ gameoflife/src/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Grid extends React.Component {
+	render () {
+		return (
+			<div>
+			Grid
+			</div>
+		);
+	}
+}
+
+class Main extends React.Component {
+
+	constructor () {
+		super();
+		this.state = {
+			generation: 0,
+		}
+	}
+
+	render() {
+		return(
+			<div>
+				<h1> The Game of Life</h1>
+				<Grid/>
+				<h2>Generations: {this.state.generation}</h2>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
+
